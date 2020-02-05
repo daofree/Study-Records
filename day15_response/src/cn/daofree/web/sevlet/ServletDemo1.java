6bf77a0c8ca9ebdc6db9a62d2dbf0a3e02784231
daofree <2949483857@qq.com>
2020-02-05 14:14:00 +0800

    BeanUtils.setProperty，登录案例没练习

---
+++ day15_response/src/cn/daofree/web/sevlet/ServletDemo1.java
@@ -0,0 +1,14 @@
+package cn.daofree.web.sevlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @ClassName ServletDemo1
+ * @Description: TODO
+ * @Author DaoTianXia
+ * @Date 2020-02-05-12:20
+ * @Version V1.0
+ **/
+public class ServletDemo1 extends HttpServlet {
+
+}
