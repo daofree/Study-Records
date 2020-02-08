5c63673431732f72534d3879d70cbc35b36e8fa0
daofree <2949483857@qq.com>
2020-02-08 14:15:51 +0800

    EL与JSTL案例，table里遍历展示list<User>

--- day17_el&jstl/src/cn/daofree/domain/User.java
+++ day17_el&jstl/src/cn/daofree/domain/User.java
@@ -16,6 +16,15 @@ public class User {
     private int age;
     private Date birthday;
 
+    public User(String name, int age, Date birthday) {
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+    }
+
+    public User() {
+    }
+
     /**
      *
      * @Description:
