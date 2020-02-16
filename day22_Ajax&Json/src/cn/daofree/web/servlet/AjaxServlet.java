b0bf5b1ace77b749e171f8c503d76c8d38cabf3d
daofree <2949483857@qq.com>
2020-02-16 22:08:52 +0800

    原生的JS实现Ajax,参考W3C

---
+++ day22_Ajax&Json/src/cn/daofree/web/servlet/AjaxServlet.java
@@ -0,0 +1,42 @@
+package cn.daofree.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName AjaxServlet
+ * @Description: TODO
+ * @Author DaoTianXia
+ * @Date 2020-02-16-21:32
+ * @Version V1.0
+ **/
+@WebServlet("/ajaxServlet")
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // 1.获取请求参数
+        String username = request.getParameter("username");
+
+        // 处理业务逻辑。耗时，
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 2.打印
+        System.out.println(username);
+
+        // 3.响应
+        response.getWriter().write("hello" + username);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
