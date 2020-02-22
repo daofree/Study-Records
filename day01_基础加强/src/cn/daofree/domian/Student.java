d0f08234eeccb56666080f8661b2e3e4b281d84c
daofree <2949483857@qq.com>
2020-02-23 00:09:55 +0800

    模拟框架原理，配置文件里的全类名===>>反射机制

---
+++ day01_基础加强/src/cn/daofree/domian/Student.java
@@ -0,0 +1,14 @@
+package cn.daofree.domian;
+
+/**
+ * @ClassName Student
+ * @Description: TODO
+ * @Author DaoTianXia
+ * @Date 2020-02-22-23:43
+ * @Version V1.0
+ **/
+public class Student {
+    public void sleep(){
+        System.out.println("sleep....");
+    }
+}
