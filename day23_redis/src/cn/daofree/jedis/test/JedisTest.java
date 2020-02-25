6327ae3ca831aafd5dce09332875a0c07b14847e
daofree <2949483857@qq.com>
2020-02-25 11:06:22 +0800

    用java操作redis5种数据类型

---
+++ day23_redis/src/cn/daofree/jedis/test/JedisTest.java
@@ -0,0 +1,168 @@
+package cn.daofree.jedis.test;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @ClassName JedisTest
+ * @Description: Jedis的Test类
+ * @Author DaoTianXia
+ * @Date 2020-02-25-0:50
+ * @Version V1.0
+ **/
+public class JedisTest {
+
+    /**
+     *
+     * @Description:
+     *         快速入门
+     */
+    @Test
+    public void test1(){
+        // 获取连接
+        Jedis jedis = new Jedis("localhost",6379);
+        // 操作
+        jedis.set("username","zhangsan");
+        // 关闭连接
+        jedis.close();
+    }
+
+    /**
+     *
+     * @Description:
+     *         字符串数据结构操作
+     */
+    @Test
+    public void test2(){
+        // 获取连接，如果使用空参构造，默认值 "localhost",6379端口
+        Jedis jedis = new Jedis();
+        //2. 操作
+        //存储
+        jedis.set("username","zhangsan");
+        //获取
+        String username = jedis.get("username");
+        System.out.println(username);
+
+        //可以使用setex()方法存储可以指定过期时间的 key value
+        //将activecode：hehe键值对存入redis，并且20秒后自动删除该键值对
+        // 激活码 验证码
+        jedis.setex("activecode",20,"hehe");
+
+        //3. 关闭连接
+        jedis.close();
+    }
+
+
+    /**
+     * hash 数据结构操作---------字符串对应map集合（对象的数据）
+     */
+    @Test
+    public void test3(){
+        //1. 获取连接
+        Jedis jedis = new Jedis();//如果使用空参构造，默认值 "localhost",6379端口
+        //2. 操作
+        // 存储hash
+        jedis.hset("user","name","lisi");
+        jedis.hset("user","age","23");
+        jedis.hset("user","gender","female");
+
+        // 获取hash
+        String name = jedis.hget("user", "name");
+        System.out.println(name);
+
+
+        // 获取hash的所有map中的数据
+        Map<String, String> user = jedis.hgetAll("user");
+
+        // keyset
+        Set<String> keySet = user.keySet();
+        for (String key : keySet) {
+            //获取value
+            String value = user.get(key);
+            System.out.println(key + ":" + value);
+        }
+
+        //3. 关闭连接
+        jedis.close();
+    }
+
+
+    /**
+     * list 数据结构操作
+     */
+    @Test
+    public void test4(){
+        //1. 获取连接
+        Jedis jedis = new Jedis();//如果使用空参构造，默认值 "localhost",6379端口
+        //2. 操作
+        // list 存储
+        jedis.lpush("mylist","a","b","c");//从左边存
+        jedis.rpush("mylist","a","b","c");//从右边存
+
+        // list 范围获取
+        List<String> mylist = jedis.lrange("mylist", 0, -1);
+        System.out.println(mylist);
+
+        // list 弹出
+        String element1 = jedis.lpop("mylist");//c
+        System.out.println(element1);
+
+        String element2 = jedis.rpop("mylist");//c
+        System.out.println(element2);
+
+        // list 范围获取
+        List<String> mylist2 = jedis.lrange("mylist", 0, -1);
+        System.out.println(mylist2);
+
+        //3. 关闭连接
+        jedis.close();
+    }
+
+    /**
+     * set 数据结构操作
+     */
+    @Test
+    public void test5(){
+        //1. 获取连接
+        //如果使用空参构造，默认值 "localhost",6379端口
+        Jedis jedis = new Jedis();
+        //2. 操作
+        // set 存储
+        jedis.sadd("myset","java","php","c++");
+
+        // set 获取
+        Set<String> myset = jedis.smembers("myset");
+        System.out.println(myset);
+
+        //3. 关闭连接
+        jedis.close();
+    }
+
+
+    /**
+     * sortedset 数据结构操作
+     */
+    @Test
+    public void test6(){
+        //1. 获取连接
+        Jedis jedis = new Jedis();//如果使用空参构造，默认值 "localhost",6379端口
+        //2. 操作
+        // sortedset 存储------------英雄出场率
+        jedis.zadd("mysortedset",3,"亚瑟");
+        jedis.zadd("mysortedset",30,"后裔");
+        jedis.zadd("mysortedset",55,"孙悟空");
+
+        // sortedset 获取
+        Set<String> mysortedset = jedis.zrange("mysortedset", 0, -1);
+
+        System.out.println(mysortedset);
+
+
+        //3. 关闭连接
+        jedis.close();
+    }
+}
--- web笔记/redis笔记.md
+++ web笔记/redis笔记.md
@@ -251,4 +251,99 @@
 ###                不进行持久化===>>>redis就是一个相当大的map集合！！！
                 
     					
-	
+	5. Java客户端 Jedis
+		* Jedis: 一款java操作redis数据库的工具.类似JDBC
+		* 使用步骤：
+			1. 下载jedis的jar包
+			2. 使用
+				//1. 获取连接
+        		Jedis jedis = new Jedis("localhost",6379);
+       			//2. 操作
+       			jedis.set("username","zhangsan");
+        		//3. 关闭连接
+        		jedis.close();
+        		
+        		
+    * Jedis操作各种redis中的数据结构
+    
+			1) 字符串类型 string
+				set
+				get
+				
+		        //2. 操作
+		        //存储
+		        jedis.set("username","zhangsan");
+		        //获取
+		        String username = jedis.get("username");
+		
+		        //可以使用setex()方法存储可以指定过期时间的 key value
+		        jedis.setex("activecode",20,"hehe");//将activecode：hehe键值对存入redis，并且20秒后自动删除该键值对
+		
+
+			2) 哈希类型 hash ： map格式  
+				hset
+				hget
+				hgetAll
+				
+		        //2. 操作
+		        // 存储hash
+		        jedis.hset("user","name","lisi");
+		        jedis.hset("user","age","23");
+		        jedis.hset("user","gender","female");
+		        
+		        // 获取hash
+		        String name = jedis.hget("user", "name");
+		        
+		        // 获取hash的所有map中的数据
+		        Map<String, String> user = jedis.hgetAll("user");
+		        
+		        // keyset
+		        Set<String> keySet = user.keySet();
+		        for (String key : keySet) {
+		            //获取value
+		            String value = user.get(key);
+		            System.out.println(key + ":" + value);
+		        }
+                
+			3) 列表类型 list ： linkedlist格式。支持重复元素
+				lpush / rpush
+				lpop / rpop
+				lrange start end : 范围获取
+				
+		        //2. 操作
+		        // list 存储
+		        jedis.lpush("mylist","a","b","c");//从左边存
+		        jedis.rpush("mylist","a","b","c");//从右边存
+		        
+		        // list 范围获取
+		        List<String> mylist = jedis.lrange("mylist", 0, -1);
+		        
+		        // list 弹出
+		        String element1 = jedis.lpop("mylist");//c
+		        String element2 = jedis.rpop("mylist");//c
+		
+		        // list 范围获取
+		        List<String> mylist2 = jedis.lrange("mylist", 0, -1);
+
+
+			4) 集合类型 set  ： 不允许重复元素
+				sadd
+				smembers:获取所有元素
+
+		        // set 存储
+		        jedis.sadd("myset","java","php","c++");
+		
+		        // set 获取
+		        Set<String> myset = jedis.smembers("myset");
+		        
+			5) 有序集合类型 sortedset：不允许重复元素，且元素有顺序
+				zadd
+				zrange
+		        //2. 操作
+		        // sortedset 存储
+		        jedis.zadd("mysortedset",3,"亚瑟");
+		        jedis.zadd("mysortedset",30,"后裔");
+		        jedis.zadd("mysortedset",55,"孙悟空");
+		        
+		        // sortedset 获取
+		        Set<String> mysortedset = jedis.zrange("mysortedset", 0, -1);			
